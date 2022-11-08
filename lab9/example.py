14250f404415f9b0f6dc75f443fd2235b204cc07
Yegoridze <yegor.chepurov@gmail.com>
2022-11-08 12:03:09 +0300

    Всё работает в исправленной программе gun, осталось сделать стены

--- lab9/example.py
+++ lab9/example.py
@@ -1,13 +1,12 @@
-class Dog:
-    def say_gaw(self):
-        if self.angry:
-            print('GAW-GAW')
-        else:
-            print('Gaw-gaw')
+def draw(self):
+    x11 = 40 - 5 * math.sin(self.an)
+    y11 = 450 + 5 * math.cos(self.an)
+    x12 = 40 + 5 * math.sin(self.an)
+    y12 = 450 - 5 * math.cos(self.an)
 
-    def ping(self):
-        self.angry = True
+    x21 = 40 + 30 * math.cos(self.an) - 5 * math.sin(self.an)
+    y21 = 450 - 30 * math.sin(self.an) + 5 * math.sin(self.an)
+    x22 = 40 + 30 * math.cos(self.an) + 5 * math.sin(self.an)
+    y22 = 450 + 30 * math.sin(self.an) - 5 * math.sin(self.an)
+    pygame.draw.polygon(self.screen, (255, 255, 0), [[x11, y11], [x12, y12], [x21, y21], [x22, y22]])
 
-    def feed(self, food_count):
-        if food_count > 10:
-            self.angry = False
